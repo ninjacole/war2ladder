616a6f00f89360d0ca4d63db960dc92495bda13e
Matt <vartanian.matt@gmail.com>
2025-11-18 08:15:11 -0500

    updates to main menu and logo

--- War2Ladder/Areas/Frontend/src/components/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export const Background = () => {
-    return <img src={"/static/blueorc.png"} alt="War2Ladder Logo" style={{ width: 150 }} />;
-}
--- War2Ladder/Areas/Frontend/src/layout/Menu.ts
+++ War2Ladder/Areas/Frontend/src/layout/Menu.ts
@@ -2,6 +2,22 @@ import { IMenuNodeProps } from "./MenuNode";
 
 export const MENU: IMenuNodeProps[] = [
     {
+        id: 'sharkfight-ladder-2025',
+        label: 'Sharkfight Ladder 2025',
+        children: [
+            { id: 'ladder-standings', label: 'Standings', href: '#' },
+            { id: 'ladder-rules', label: 'Rules', href: '#' }
+        ]
+    },
+    {
+        id: 'events',
+        label: 'Events',
+        children: [
+            { id: 'upcoming', label: 'Upcoming', href: '#' },
+            { id: 'past', label: 'Past Events', href: '#' }
+        ]
+    },
+    {
         id: 'wc2players',
         label: 'Warcraft 2 Players',
         children: [
@@ -10,30 +26,13 @@ export const MENU: IMenuNodeProps[] = [
             { id: 'players-search', label: 'Search', href: '#' }
         ]
     },
-    { id: 'form', label: 'Form', href: '#' },
+    { id: 'forum', label: 'Forum', href: '#' },
     {
         id: 'maps',
         label: 'Maps',
         children: [
             { id: 'map-browser', label: 'Map Browser', href: '#' },
             { id: 'upload-map', label: 'Upload Map', href: '#' }
-        ]
-    },
-    {
-        id: 'sharkfight-ladder-2025',
-        label: 'Sharkfight Ladder 2025',
-        children: [
-            { id: 'ladder-standings', label: 'Standings', href: '#' },
-            { id: 'ladder-rules', label: 'Rules', href: '#' }
-        ]
-    },
-    { id: 'sharkfight-records', label: 'Sharkfight Records', href: '#' },
-    {
-        id: 'events',
-        label: 'Events',
-        children: [
-            { id: 'upcoming', label: 'Upcoming', href: '#' },
-            { id: 'past', label: 'Past Events', href: '#' }
         ]
     },
     { id: '100prize', label: '$100 Prize Draw', href: '#' },
